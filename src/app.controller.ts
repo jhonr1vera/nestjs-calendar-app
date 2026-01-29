1c23178d91a372ab2dc5b9f2f0a785b749f1c24d
Jhon Rivera <jhonariverab@gmail.com>
2026-01-28 20:53:57 -0400

    feat!: Add views for endpoints

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,33 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render, Req, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthGuard } from './auth/auth.guard';
+import { GetUser } from './shared/decorators/get-user.decorator';
+import { Public } from './shared/decorators/public.decorator';
+import { Roles } from './shared/decorators/roles.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) { }
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @UseGuards(AuthGuard)
+  @Render('user/home')
+  getHome(@GetUser() user) {
+    return { user };
+  }
+
+  @Get('admin')
+  @Roles('admin')
+  @UseGuards(AuthGuard)
+  @Render('admin/home')
+  getAdminHome(@GetUser() user) {
+    return { user };
+  }
+
+  @Get('landing-page')
+  @Public()
+  @Render('landing-page')
+  getLandingPage() {
+    return { user: null };
   }
 }
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,12 +3,22 @@ import { AuthService } from './auth.service';
 import { LoginAuthDto } from './dto/login-auth.dto';
 import { RegisterAuthDto } from './dto/register-auth.dto';
 import { Public } from 'src/shared/decorators/public.decorator';
-import { Controller, Get, Post, Body, UseGuards, Req, Render } from '@nestjs/common';
+import { Controller, Get, Post, Body, UseGuards, Req, Render, Res } from '@nestjs/common';
 import { Roles } from 'src/shared/decorators/roles.decorator';
+import type { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) { }
+  constructor(
+    private readonly authService: AuthService,
+  ) { }
+
+
+  @Get('register')
+  @Public()
+  @Render('auth/register')
+  showRegister() {
+  }
 
   @Post('register')
   @Public()
@@ -22,10 +32,22 @@ export class AuthController {
     return this.authService.registerAdmin(RegisterAuthDto);
   }
 
+  @Get('login')
+  @Public()
+  @Render('auth/login')
+  showLogin() {
+  }
+
   @Post('login')
   @Public()
-  login(@Body() LoginAuthDto: LoginAuthDto) {
-    return this.authService.localLogin(LoginAuthDto);
+  async login(@Body() LoginAuthDto: LoginAuthDto, @Res() res: Response) {
+    const token = await this.authService.localLogin(LoginAuthDto);
+    res.cookie('access_token', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      maxAge: 3600 * 1000,
+    });
+    return res.redirect('/');
   }
 
   @Get('google')
@@ -40,5 +62,15 @@ export class AuthController {
   async googleLogin(@Req() req) {
     const user = req.user;
     return this.authService.googleLogin(user);
+  }
+  @Post('logout')
+  @Public()
+  logout(@Res({ passthrough: true }) res: Response) {
+    res.clearCookie('access_token', {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+    });
+
+    return this.authService.logout();
   }
 }
