2df58c75d9146a322dc4480cee41a0481f10f3a6
Jhon Rivera <jhonariverab@gmail.com>
2026-01-28 21:00:49 -0400

    refactor: Change Auth guard for working with new token storage

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -10,7 +10,6 @@ import { Request } from 'express';
 import { Reflector } from '@nestjs/core';
 import { IS_PUBLIC_KEY } from 'src/shared/decorators/public.decorator';
 import { I18nService } from 'nestjs-i18n';
-
 import { UsersService } from 'src/users/users.service';
 
 @Injectable()
@@ -28,32 +27,43 @@ export class AuthGuard implements CanActivate {
       context.getClass(),
     ]);
 
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractToken(request);
+
+    if (token) {
+      try {
+        const payload = await this.jwtService.verifyAsync(token);
+        const user = await this.usersService.findOneByPublicId(payload.sub);
+        if (user) {
+          request['user'] = user;
+        }
+      } catch {
+        // If token is invalid, we ignore it.
+      }
+    }
+
     if (isPublic) {
       return true;
     }
 
-    const request = context.switchToHttp().getRequest();
-    const token = this.extractTokenFromHeader(request);
-    if (!token) {
+    if (!request['user']) {
       throw new UnauthorizedException(this.i18nService.t("validators.UNAUTHORIZED"));
     }
-    try {
-      const payload = await this.jwtService.verifyAsync(token);
-      const user = await this.usersService.findOneByPublicId(payload.sub);
 
-      if (!user) {
-        throw new UnauthorizedException(this.i18nService.t("validators.UNAUTHORIZED"));
-      }
-
-      request['user'] = user;
-    } catch {
-      throw new UnauthorizedException(this.i18nService.t("validators.UNAUTHORIZED"));
-    }
     return true;
   }
 
-  private extractTokenFromHeader(request: Request): string | undefined {
+  private extractToken(request: Request): string | undefined {
     const [type, token] = request.headers.authorization?.split(' ') ?? [];
-    return type === 'Bearer' ? token : undefined;
+    if (type === 'Bearer') {
+      return token;
+    }
+
+    // Check cookie
+    if (request.cookies && request.cookies['access_token']) {
+      return request.cookies['access_token'];
+    }
+
+    return undefined;
   }
 }
